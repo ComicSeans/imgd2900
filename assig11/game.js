1eb795fc27113ce2baa762447fd3e6695d6fa5e8
Sean Halloran <seanjhalloran@gmail.com>
2014-11-16 18:55:03 -0500

    adds difficulty progression and failure state

--- assig11/game.js
+++ assig11/game.js
@@ -108,18 +108,36 @@ var G;
 		width : 8,
 		height : 8,
 
+		/**
+		 * calculates tbe number of beads on the grid
+		 *
+		 * @returns {number}
+		 * 		number of beads on the grid
+		 */
 		numBeads : function(){
 			return G.width * G.height;
 		},
 
+		//color of solved tile
 		COLOR_GOOD : {r : 255, g : 255, b : 255},	//white
+		//color of unsolved tile
 		COLOR_BAD :  {r : 0,   g : 0,   b : 0},		//black
 
 		//COLOR_GOOD : {r : 255, g : 0, b : 0},
 		//COLOR_BAD :  {r : 0,   g : 0,   b : 255},
 
+		currentDifficulty : 1,
+		clicksRemaining : 1,
+
+		/**
+		 * Store the clicks to create the current puzzle
+		 */
+		currentPuzzle : [],
+
 		//the last piano note played
 		lastNote : "piano_c6",
+
+		soundFxPlaying : false,
 
 		/**
 		 * Counts the number of solved beads
@@ -236,17 +254,40 @@ var G;
 			}
 		},
 
-		generatePuzzle : function (clicks){
+		clearBoard : function(){
+			PS.gridSize(G.width, G.height);
+			PS.gridColor(G.COLOR_GOOD);
+			PS.color(PS.ALL, PS.ALL, G.COLOR_GOOD);
+			PS.border(PS.ALL, PS.ALL, 0);
+			PS.applyRect(0, 0, G.width, G.height, PS.data, {solved : true, sound : true});
+		},
+
+		/**
+		 * Generates a puzzle to solve in a number of clicks, and returns the
+		 * clicks used to generate that puzzle.
+		 *
+		 * @param numClicks
+		 * 		number of clicks to solve the puzzle
+		 * @returns {Array}
+		 * 		return an array of touches which are {lx : number, ly : number}
+		 */
+		generatePuzzle : function (numClicks){
+			//PS.debug("===NEW PUZZLES FROM "+numClicks+" NUMBER CLICKS===\n");
+
+			G.clearBoard();
+
+			G.updateStatusBar();
+
 			var touches = [];
 			var x = PS.random(3) + (G.width  / 2) - 2;
 			var y = PS.random(3) + (G.height / 2) - 2;
 			touches.push({lx : x, ly : y});
 			PS.touch(x, y, {sound : false});
 			var dX, dY;
-			for(var i = 1; i < clicks; i++){
+			for(var i = 1; i < numClicks; i++){
 				dX = PS.random(2) * ((PS.random(2) == 1) ? 1 : -1);
 				dY = PS.random(2) * ((PS.random(2) == 1) ? 1 : -1);
-				PS.debug("dx : "+dX+" dy: "+dY+"\n");
+				//PS.debug("dx : "+dX+" dy: "+dY+"\n");
 				x = myMath.clamp(x + dX, 1, G.width - 2);
 				y = myMath.clamp(y + dY, 1, G.height - 2);
 				if(!G.touchMade(touches, {lx : x, ly : y})){
@@ -259,8 +300,41 @@ var G;
 					PS.debug("Found same touch!\n");
 				}
 			}
+			//PS.debug("Made board with touches :\n");
+			//touches.forEach(function(entry){
+			//	PS.debug(entry.lx + "," + entry.ly +"\n");
+			//});
+			return touches;
 		},
 
+		/**
+		 * Clear's the board and creates a puzzle from a set of cliks
+		 * @param clicks
+		 * 		array of touches which are {lx : number, ly : number}
+		 */
+		generatePuzzleFromClicks : function(clicks){
+			//PS.debug("===RESETING PUZZLE FROM "+clicks.length+" CLICKS===\n");
+
+			G.clearBoard();
+
+			G.updateStatusBar();
+
+			clicks.forEach(function(entry){
+				PS.touch(entry.lx, entry.ly, {sound : false});
+			});
+
+		},
+
+		/**
+		 * Checks if a touch has been made at a location in a list of touches
+		 *
+		 * @param touches
+		 * 		array of {lx : number, ly : number}
+		 * @param touch
+		 * 		{lx : number, ly : number} to check for
+		 * @returns {boolean}
+		 * 		if touch was found in touches
+		 */
 		touchMade : function (touches, touch){
 			var found = false;
 			touches.forEach(function(entry){
@@ -270,6 +344,45 @@ var G;
 				}
 			});
 			return found;
+		},
+
+		/**
+		 * Called when board is solved
+		 */
+		onSolved : function(){
+			//PS.debug("SOLVED\n");
+			G.currentDifficulty+= 1;
+			G.clicksRemaining = G.currentDifficulty;
+			G.soundFxPlaying = true;
+			PS.audioPlay("fx_tada",
+				//play when 'tada' finishes playing
+				{onEnd : function(){
+					G.soundFxPlaying = false;
+					G.currentPuzzle = G.generatePuzzle(G.currentDifficulty);
+			}});
+		},
+
+		/**
+		 * Called when player fails to solve in number of clicks
+		 */
+		onFailed : function(){
+			//PS.debug("FAILED\n");
+			G.clicksRemaining = G.currentDifficulty;
+			G.soundFxPlaying = true;
+			PS.audioPlay("fx_wilhelm",
+				//play when 'wilhelm' finishes playing
+				{onEnd : function(){
+					G.soundFxPlaying = false;
+					G.generatePuzzleFromClicks(G.currentPuzzle);
+				}});
+		},
+
+		/**
+		 * Make the status bar read the remaining clicks
+		 */
+		updateStatusBar : function(){
+			PS.statusColor( PS.COLOR_RED );
+			PS.statusText("Click to solve in " + G.clicksRemaining + " clicks");
 		}
 
 	};
@@ -292,31 +405,21 @@ PS.init = function( system, options ) {
 	// Do this FIRST to avoid problems!
 	// Otherwise you will get the default 8x8 grid
 
-	PS.gridSize(G.width, G.height);
-	PS.gridColor(G.COLOR_GOOD);
-	PS.color(PS.ALL, PS.ALL, G.COLOR_GOOD);
-	PS.border(PS.ALL, PS.ALL, 0);
-	PS.applyRect(0, 0, G.width, G.height, PS.data, {solved : true, sound : true});
-
-	PS.statusColor( PS.COLOR_RED );
-	PS.statusText( "Touch the screen" );
-
 	PS.audioLoad( "piano_c6", { lock: true } ); // load & lock click sound
 	PS.audioLoad( "piano_c5", { lock: true } ); // load & lock click sound
 
-	G.generatePuzzle(5);
+	G.currentPuzzle = G.generatePuzzle(G.currentDifficulty);
 
-	//var touches = [];
-	//touches.push({lx : 1, ly : 1});
-	//touches.push({lx : 2, ly : 2});
-	//touches.push({lx : 1, ly : 1});
-    //
-	//if(G.touchMade(touches, {lx : 1, ly : 1})){
-	//	PS.debug("found it!");
-	//}
-	//else{
-	//	PS.debug("nope!");
-	//}
+	//PS.debug("Made board with touches :\n");
+	//G.currentPuzzle.forEach(function(entry){
+	//	PS.debug(entry.lx + "," + entry.ly +"\n");
+	//});
+
+	//G.generatePuzzleFromClicks([
+	//	{lx : 3, ly : 3},
+	//	{lx : 5, ly : 5},
+	//	{lx : 6, ly : 6}
+	//])
 
 	// Add any other initialization code you need here
 };
@@ -333,15 +436,17 @@ PS.init = function( system, options ) {
 PS.touch = function( x, y, data, options ) {
 	"use strict";
 
-	PS.debug( "PS.touch() @ " + x + ", " + y + "\n" );
+	//PS.debug( "PS.touch() @ " + x + ", " + y + "\n" );
 
-	//x = myMath.clamp(x, 0, G.width);
-	//y = myMath.clamp(y, 0, G.height);
+	if(G.soundFxPlaying)
+	{
+		return;
+	}
 
 	//toggle the clicked and adjacent beads
-	for(var i = myMath.clamp(x - 1, 0, 8); i < myMath.clamp(x + 2, 0, 8); i++)
+	for(var i = myMath.clamp(x - 1, 0, G.width); i < myMath.clamp(x + 2, 0, G.width); i++)
 	{
-		for(var j = myMath.clamp(y - 1, 0, 8); j < myMath.clamp(y + 2, 0, 8); j++)
+		for(var j = myMath.clamp(y - 1, 0, G.height); j < myMath.clamp(y + 2, 0, G.height); j++)
 		{
 			G.toggleBead(i, j);
 		}
@@ -349,22 +454,27 @@ PS.touch = function( x, y, data, options ) {
 
 	//Play a note
 	if(data.sound) {
-		PS.debug("Playing sound\n");
 		G.playToggleSound();
-	}
-	else{
-		PS.debug("Not playing sound\n");
 	}
 
 	//Adjust the background color of the screen for how lit up the grid is
 	G.updateBackgroundColor();
 
-	//play ta-da if the whole screen is lit up
-	if(G.allSolved())
-	{
-		PS.audioPlay("fx_tada");
-	}
 
+
+	if(data.sound) {
+		G.clicksRemaining--;
+
+		G.updateStatusBar();
+
+		//play ta-da if the whole screen is lit up
+		if (G.allSolved()) {
+			G.onSolved();
+		}
+		else if (G.clicksRemaining <= 0) {
+			G.onFailed();
+		}
+	}
 };
 
 // PS.release ( x, y, data, options )
@@ -400,7 +510,7 @@ PS.enter = function( x, y, data, options ) {
 
 	// Add code here for when the mouse cursor/touch enters a bead
 
-	PS.alpha(x, y, PS.ALPHA_OPAQUE - 100);
+	PS.alpha(x, y, PS.ALPHA_OPAQUE - 140);
 
 };
 
